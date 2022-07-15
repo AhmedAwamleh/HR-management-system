03903d5e399ff4453eb01757e2a8bfac8a40c21c
AhmedAwamleh <awamlehad@icloud.com>
2022-07-15 14:10:50 +0300

    create a prototype

--- script.js
+++ script.js
@@ -1,55 +1,65 @@
 
-const allEmployee= [];
+const allEmployee = [];
 
 function Employee(EmployeeID,FullName,Department,Level,Image){
  
-  this.EmployeeID=EmployeeID
+  this.EmployeeID=EmployeeID;
   
-  this.FullName=FullName
+  this.FullName=FullName;
     
-  this.Department=Department
+  this.Department=Department;
 
-  this.Level=Level
+  this.Level=Level;
 
-  this.Image=Image
+  this.Image=Image;
 
-  
- 
+
   allEmployee.push(this);
+}
 
-  function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min};
+
+
+Employee.prototype.salary = function () {
+    (this.Level.toLowerCase() === "Senior") ? this.salary =  getRndIntegar(1500, 2000):
+        (this.Level.toLowerCase() === "Mid-Senior") ? this.salary =  getRndIntegar(100,1500) :
+            this.salary =  getRndIntegar(500, 1000)
 
 }
-Employee.prototype.Salary=function(){
 
-    (this.Level == "Senior")?this.Salary=getRndInteger(1500,2000):
-    (this.Level==="Mid-Senior")?this.Salary=getRndInteger(1000,1500):
-     this.Salary=getRndInteger(500,1000);
-    
-     allEmployee.push(this.Salary);
-
+Employee.prototype.netsalary = function () {
+    this.netsalary = Math.floor(this.salary - (this.salary * 0.075));
 }
+
+
+
+ 
+
 
 Employee.prototype.Nameandsalary=function(){
-    console.log(`${this.FullName}; ${this.Salary} jd \n`);
-    document.write(`<p>${this.FullName}; ${this.Salary} jd\n</p>`);
+    console.log(`${this.FullName}; ${this.salary} jd \n`);
+    document.write(`<p>${this.FullName}; ${this.salary};this net salary    ${this.netsalary} jd\n</p>`);
 
 }
 
 
+const GhaziSamer=new Employee("1000","GhaziSamer","Administration","Senior","image/Ghazi.jpg")
+const GhazLana=new Employee("1001","LanaAli","Finance","Senior","image/Ghazi.jpg")
+const TamaraAyoub=new Employee("1002","TamaraAyoub","Marketing","Senior","image/Ghazi.jpg")
+const SafiWalid=new Employee("1003","SafiWalid","Administration","Mid-Senior","image/Ghazi.jpg")
+const OmarZaid=new Employee("1004","OmarZaid","Development","Senior","image/Ghazi.jpg")
+const RanaSaleh=new Employee("1005","RanaSaleh","Development","Junior","image/Ghazi.jpg")
+const HadiAhmad=new Employee("1006","HadiAhmad","Finance","Mid-Senior","image/Ghazi.jpg")
 
-const GhaziSamer=new Employee(1000,"GhaziSamer","Administration","Senior","image/Ghazi.jpg")
-const GhazLana=new Employee(1001,"LanaAli","Finance","Senior","image/Ghazi.jpg")
-const TamaraAyoub=new Employee(1002,"TamaraAyoub","Marketing","Senior","image/Ghazi.jpg")
-const SafiWalid=new Employee(1003,"SafiWalid","Administration","Mid-Senior","image/Ghazi.jpg")
-const OmarZaid=new Employee(1004,"OmarZaid","Development","Senior","image/Ghazi.jpg")
-const RanaSaleh=new Employee(1005,"RanaSaleh","Development","Junior","image/Ghazi.jpg")
-const HadiAhmad=new Employee(1006,"HadiAhmad","Finance","Mid-Senior","image/Ghazi.jpg")
 
-for(let i=0;i<allEmployee.length;i++){
+
+function getRndIntegar(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;}
+
+for (let i = 0; i < allEmployee.length; i++) {
+    
+console.log(allEmployee[i]);
+    allEmployee[i].salary();
+    allEmployee[i].netsalary();
     allEmployee[i].Nameandsalary();
-   
-    allEmployee[i].Salary();
-
 }
+
